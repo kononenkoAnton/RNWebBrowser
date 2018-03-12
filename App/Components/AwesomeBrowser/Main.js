13cd5e7fc04493118cbf032c8162cf9880d4329f
François Roland <f.roland@applicaster.com>
2018-03-12 16:58:11 +0100

    use class properties and arrow functions to avoid the need of `.bind(this)` when passing component methods as props

--- App/Components/AwesomeBrowser/Main.js
+++ App/Components/AwesomeBrowser/Main.js
@@ -2,90 +2,83 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Toolbar from './Toolbar';
 
-import {
-    View,
-    WebView,
-    StyleSheet,
-    StatusBar
-  } from 'react-native';
+import { View, WebView, StyleSheet, StatusBar } from 'react-native';
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#F6F6EF',
-        flexDirection: 'column'
-      },
-    webView: {
-        flex:1
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: '#F6F6EF',
+    flexDirection: 'column',
+  },
+  webView: {
+    flex: 1,
+  },
+});
 const WEBVIEW_REF = 'webview';
 const TOOLBAR_REF = 'toolbar';
 
-export default class Main extends Component {    
-    constructor(props) {
-        super(props);
-        this.state = {
-            canGoBack: false,
-            canGoForward: false,
-            };        
-      }
+export default class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      canGoBack: false,
+      canGoForward: false,
+    };
+  }
 
-    reloadWebView() {
-        this.refs[WEBVIEW_REF].reload();
-    }
-    onBack() {
-        this.refs[WEBVIEW_REF].goBack();
-      }
+  reloadWebView = () => {
+    this.refs[WEBVIEW_REF].reload();
+  };
+  onBack = () => {
+    this.refs[WEBVIEW_REF].goBack();
+  };
 
-    onForward() {
-        this.refs[WEBVIEW_REF].goForward();
-    }
+  onForward = () => {
+    this.refs[WEBVIEW_REF].goForward();
+  };
 
-    onNavigationStateChange(navState) {
-        this.setState({
-          canGoBack: navState.canGoBack,
-          canGoForward: navState.canGoForward
-          
-        });
-      }
+  onNavigationStateChange = navState => {
+    this.setState({
+      canGoBack: navState.canGoBack,
+      canGoForward: navState.canGoForward,
+    });
+  };
 
-    render() {
-        const statusBarHidden = true     
-        console.log(this.state.canGoBack)
-        
-      return (
-        <View style={styles.container}>
-          
-                <StatusBar
-                    backgroundColor="blue"
-                    barStyle="dark-content"
-                    hidden={statusBarHidden}
-                />
+  render() {
+    const statusBarHidden = true;
+    console.log(this.state.canGoBack);
 
-                <Toolbar 
-                    canGoBack={ this.state.canGoBack}
-                    canGoForward={ this.state.canGoForward}
-                    goWebViewBack= {this.onBack}
-                    goWebViewForward= {this.onForward}
-                    goWebViewRefresh= {this.reloadWebView}
-                    uri= { this.props.uri }/>
-                <WebView
-                    ref={WEBVIEW_REF}
-                    source={{ uri: this.props.uri }}
-                    onNavigationStateChange={this.onNavigationStateChange.bind(this)}
-                    style={ styles.webView }
-                />
-            </View>
-      );
-    }
+    return (
+      <View style={styles.container}>
+        <StatusBar
+          backgroundColor="blue"
+          barStyle="dark-content"
+          hidden={statusBarHidden}
+        />
+
+        <Toolbar
+          canGoBack={this.state.canGoBack}
+          canGoForward={this.state.canGoForward}
+          goWebViewBack={this.onBack}
+          goWebViewForward={this.onForward}
+          goWebViewRefresh={this.reloadWebView}
+          uri={this.props.uri}
+        />
+        <WebView
+          ref={WEBVIEW_REF}
+          source={{ uri: this.props.uri }}
+          onNavigationStateChange={this.onNavigationStateChange}
+          style={styles.webView}
+        />
+      </View>
+    );
+  }
 }
 
 Main.propTypes = {
-    uri: PropTypes.string.isRequired,  
-};
-  
-Main.defaultProps = {
-    uri: 'https://google.com'
+  uri: PropTypes.string.isRequired,
 };
 
+Main.defaultProps = {
+  uri: 'https://google.com',
+};
